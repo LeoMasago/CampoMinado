86dc9318277421b2cd3e33254928fcfb880c6bc4
LeoMasago <101646035+LeoMasago@users.noreply.github.com>
2023-03-17 11:27:26 -0300

    1.1

---
+++ CampoMinado/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampoMinado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = { 1, 2, 3, 4, 5 };
+
+            int linha;
+            int coluna;
+            int tentativas;
+
+            tentativas = 2;
+
+            while {
+                     Console.WriteLine("", " 1  2  3  4  5 ", " ");
+                     Console.WriteLine("- - - - - - - - ");
+
+                for linha 
+
+
+
+
+
+            }
+        }
+    }
+}
